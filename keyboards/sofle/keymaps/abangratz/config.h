70fcd3ee629da00c1aca385f26550dd558eb0c25
Anton Bangratz <anton@platogo.com>
2022-02-27 11:36:33 +0100

    Add: New keymap by abangratz

---
+++ keyboards/sofle/keymaps/abangratz/config.h
@@ -0,0 +1,8 @@
+#pragma once
+#define PERMISSIVE_HOLD
+#define TAPPING_TOGGLE 2
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
